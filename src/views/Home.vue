98ad4cb3260f006617aff8a2dbe6aae642e3407d
whatisatanner <tblackwell@icloud.com>
2023-08-01 17:35:26 -0400

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+  <div class="h-screen w-screen flex flex-col place-content-center place-items-center bg-neutral-400">
+    <h1 class="text-4xl font-semibold tracking-wide">Welcome</h1>
+        <p class="pb-5">
+      This project is set up with tailwind and vue-router.
+    </p>
+    <div class="max-w-prose">
+
+
+
+    <p>
+      <b>To setup your Git Hub repository<br>execute the following commands:</b>
+      <ul>
+        <li>git init</li>
+        <li>gh repo create</li>
+      </ul>
+    </p>
+
+    <p class="pt-5">
+      <b>To deploy with Netlify<br>execute the following commands:</b>
+      <ul>
+        <li>netlify init</li>
+      </ul>
+    </p>  
+    </div>
+    
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
